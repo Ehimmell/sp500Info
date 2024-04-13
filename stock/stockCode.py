8bc26f3e7f4194a260f44fb822ac851ef1b6c6cf
Ehimmell <himmellethan@gmail.com>
2024-04-13 14:48:35 -0500

    add average change

--- stock/stockCode.py
+++ stock/stockCode.py
@@ -11,9 +11,6 @@ with open('sp500_model.pkl', 'rb') as file:
 sp500 = yf.Ticker("^GSPC")
 sp500 = sp500.history(period="10y")
 
-del sp500["Dividends"]
-del sp500["Stock Splits"]
-
 horizons = [2,5,60,250,1000]
 new_predictors = []
 
@@ -46,6 +43,14 @@ def whatToDo():
     if(prediction == 1): return "BUY!!!!!! NOW!!!!!!!"
     return "SELL SELL SELL!!!!!!!!!!!!!"
 
+def derivative():
+    daysPrevious = input("How many days back would you like to see the average change? ")
+    data = sp500.iloc[-int(daysPrevious):].copy()
+    data["Daily Change"] = (data["Close"] - data["Open"]).astype(float)
+    return data["Daily Change"].mean()
+
 print("Prediction for tomorrow's stock:", whatToDo())
+
+print("Average change in stock price over the last x days: ", derivative())
 
 
