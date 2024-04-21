30b73cda10d30daa90b0e210f23794e2aa6a339f
Ehimmell <himmellethan@gmail.com>
2024-04-20 23:58:50 -0500

    stuff

---
+++ stockCode/python-scripts/statMaker.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import dataInteract
+
+def trendGraph(timeFrame):
+
+    sp500 = dataInteract.getDailyStock()
+
+    sp500 = sp500.iloc[-timeFrame]
+
+    sp500[f'Trend_{timeFrame}'].plot(figsize=(10, 6))
+
+    plt.title(f'S&P 500 Day Trend Over {timeFrame} Days')
+
+    plt.show()
+
+def priceGraph(timeFrame):
+
+    sp500 = dataInteract.getDailyStock()
+
+    sp500 = sp500.iloc[-timeFrame:]
+
+    sp500['Close'].plot(figsize=(10, 6))
+
+    plt.title(f'S&P 500 Price Over {timeFrame}  Days')
+
+    plt.show()
+
+
