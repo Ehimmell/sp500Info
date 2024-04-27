2e2f213e37dcc4d9157e6f105672461058bd4651
Ethan Himmell <himme@MacBook-Pro.local>
2024-04-27 15:39:39 -0500

    third round

---
+++ public/python-scripts/testDB.py
@@ -0,0 +1,48 @@
+# Description: This file contains the unit tests for the database connection and the existence of the tables in the database.
+import os
+import unittest
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+import constants
+import pandas as pd
+
+class TestDBConnection(unittest.TestCase):
+
+    #set up the test
+    def setUp(self):
+        DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
+        self.engine = create_engine(DATABASE_URL)
+
+    #tear down the test
+    def tearDown(self):
+        self.engine.dispose()
+
+    #test the database connection
+    def test_db_connection(self):
+        try:
+            connection = self.engine.connect()
+            self.assertTrue(connection is not None)
+        except OperationalError:
+            self.fail("Could not establish a connection to the database")
+
+    #test the existence of the sp500 table
+    def test_sp500_table(self):
+        try:
+            connection = self.engine.connect()
+            result = pd.read_sql_query(f"SELECT * FROM {constants.SP500_TABLE}", connection)
+            self.assertTrue(result is not None)
+        except OperationalError:
+            self.fail("Could not establish a connection to the database")
+
+    #test the existence of the stockpred table
+    def test_stockpred_table(self):
+        try:
+            connection = self.engine.connect()
+            result = pd.read_sql_query(f"SELECT * FROM {constants.STOCKPRED_TABLE}", connection)
+            self.assertTrue(result is not None)
+        except OperationalError:
+            self.fail("Could not establish a connection to the database")
+
+#run the tests
+if __name__ == '__main__':
+    unittest.main()
