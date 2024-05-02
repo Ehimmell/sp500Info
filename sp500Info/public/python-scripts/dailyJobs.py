96f48f9f209e1039794df2819b39a2ee20922ea5
Ehimmell <himmellethan@gmail.com>
2024-05-02 17:01:52 -0500

    add dysfunctional model, needs more data

--- sp500Info/public/python-scripts/dailyJobs.py
+++ sp500Info/public/python-scripts/dailyJobs.py
@@ -5,6 +5,6 @@ import newsScraper
 #Scrape the news
 newsScraper.scrape()
 #Send the daily stock data to heroku postgres db
-dataInteract.sendDailyStock()
+#dataInteract.sendDailyStock()
 #Send the daily prediction to heroku postgres db
-dataInteract.sendDailyPrediction()
+#dataInteract.sendDailyPrediction()
