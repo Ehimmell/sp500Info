76f04df231d7cdad26957a574d602eb4b0fb0e81
Ehimmell <himmellethan@gmail.com>
2024-05-02 19:50:42 -0500

    more scrapers

--- sp500Info/public/python-scripts/dailyJobs.py
+++ sp500Info/public/python-scripts/dailyJobs.py
@@ -2,9 +2,34 @@
 import dataInteract
 import newsScraper
 
+scrape = input("What news sources would you like to scrape? Seperate by comma. ex: <wsj, yfinance>(wsj, cnn, yfinance, or all): ")
+sendStock = input("Would you like to send the daily stock data to the db? (y or n): ")
+sendPrediction = input("Would you like to send the daily prediction to the db? (y or n): ")
 #Scrape the news
-newsScraper.scrape()
+while(len(scrape) > 2):
+    nextComma = scrape.find(',')
+    if(nextComma != -1): input = scrape[:nextComma]
+    else: input = scrape
+
+    if(input == 'wsj'):
+        newsScraper.scrapeWSJ()
+    elif(input == 'cnn'):
+        newsScraper.scrapeCNNBiz()
+    elif(input == 'yfinance'):
+        newsScraper.scrapeYahooFinance()
+    elif(input == 'all'):
+        newsScraper.scrapeWSJ()
+        newsScraper.scrapeCNNBiz()
+        newsScraper.scrapeYahooFinance()
+    else:
+        print("Invalid input")
+        break
+
+    if(nextComma != -1) :scrape = scrape[nextComma+1:]
+    else: scrape = ''
 #Send the daily stock data to heroku postgres db
-#dataInteract.sendDailyStock()
+if(sendStock == 'y'):
+    dataInteract.sendDailyStock()
 #Send the daily prediction to heroku postgres db
-#dataInteract.sendDailyPrediction()
+if(sendPrediction == 'y'):
+    dataInteract.sendDailyPrediction()
--- sp500Info/public/python-scripts/newsScraper.py
+++ sp500Info/public/python-scripts/newsScraper.py
@@ -9,7 +9,7 @@ import dataPrep
 import constants
 
 #Method to scrape the wsj for headlines
-def scrape():
+def scrapeWSJ():
 
     #get the response from the wsj
     response = rq.get(constants.WSJ_URL, headers=constants.REQ_HEADERS)
@@ -64,3 +64,101 @@ def scrape():
             now = datetime.datetime.now()
             writer.writerow([now.strftime("%Y-%m-%d %H:%M:%S")])
             writer.writerow(["size: " + str(len(existing_articles))])
+
+def scrapeCNNBiz():
+    #get the response from the wsj
+    response = rq.get(constants.CNN_URL, headers=constants.REQ_HEADERS)
+
+
+    #if the response is good, parse the response and get the headlines
+    if response.status_code == 200:
+
+        #parse the response
+        clean = bs(response.text, 'lxml')
+
+        #initilize empty articles array for later
+        articles = []
+
+        for item in clean.select(f'span.{constants.HEADLINE_TEXT}'):
+            headline = item.get_text()
+            articles += [headline]
+
+        #get the existing articles
+
+        with open(constants.HEADLINES_PATH, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            existing_articles = list(reader)
+
+        #flatten the existing articles
+
+        existing_articles = [item for sublist in existing_articles for item in sublist]
+
+        #clean the articles by checking if an article that was scraped is already in the existing articles
+
+        for article in articles:
+            if article not in existing_articles:
+                existing_articles.append(article)
+
+        #clean the articles
+        existing_articles = dataPrep.clean(existing_articles)
+
+        #write the cleaned articles to the csv file
+        with open(constants.HEADLINES_PATH, 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            for article in existing_articles:
+                writer.writerow([article])
+            writer.writerow(["    "])
+
+        #write the runtime to the csv file
+        with open(constants.RUNTIMES_PATH, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            now = datetime.datetime.now()
+            writer.writerow([now.strftime("%Y-%m-%d %H:%M:%S")])
+            writer.writerow(["size: " + str(len(existing_articles))])
+
+def scrapeYahooFinance():
+
+    response = rq.get(constants.YFINANCE_URL, headers=constants.REQ_HEADERS)
+
+    if response.status_code == 200:
+
+        clean = bs(response.text, 'lxml')
+
+        articles = []
+
+        for item in clean.select(f'h3.{constants.CLAMP}'):
+            headline = item.get_text()
+            articles += [headline]
+
+        print(articles)
+
+        with open(constants.HEADLINES_PATH, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            existing_articles = list(reader)
+
+        existing_articles = [item for sublist in existing_articles for item in sublist]
+
+        for article in articles:
+            if article not in existing_articles:
+                existing_articles.append(article)
+
+        existing_articles = dataPrep.clean(existing_articles)
+
+        with open(constants.HEADLINES_PATH, 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            for article in existing_articles:
+                writer.writerow([article])
+            writer.writerow(["    "])
+
+        with open(constants.RUNTIMES_PATH, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            now = datetime.datetime.now()
+            writer.writerow([now.strftime("%Y-%m-%d %H:%M:%S")])
+            writer.writerow(["size: " + str(len(existing_articles))])
+
+
+scrapeYahooFinance()
+def scrapeAll():
+    scrapeWSJ()
+    scrapeCNNBiz()
+    scrapeYahooFinance()
